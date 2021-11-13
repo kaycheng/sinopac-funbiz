b1c3ee837338778ae88549852dc37226249d0c08
kaycheng <kaycheng717@gmail.com>
2021-11-13 15:19:23 +0800

    First init

---
+++ lib/sinopac/funbiz.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "funbiz/version"
+
+module Sinopac
+  module Funbiz
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/sinopac/funbiz_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Sinopac::Funbiz do
+  it "has a version number" do
+    expect(Sinopac::Funbiz::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
